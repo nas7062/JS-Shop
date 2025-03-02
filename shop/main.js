6b728ae1a4ec1b3d7c827cf8d67537bc80e509e0
10012 <nas7062@naver.com>
2025-03-02 23:33:22 +0900

    회원가입 페이지 추가 및 로그인 페이지 수정

---
+++ shop/main.js
@@ -0,0 +1,85 @@
+import './style.css';
+import CateImg from "./public/image/category.png";
+import userImg from "./public/image/user.png";
+import cartImg from "./public/image/cart.png";
+
+// 이미지 동적 추가
+document.getElementById('category-img').src = CateImg;
+document.getElementById('mypage-img').src = userImg;
+document.getElementById('cart-img').src = cartImg;
+
+// 롤링 배너 복제본 생성
+const roller = document.querySelector('.Rolling-list');
+roller.id = "roller1";
+const clone = roller.cloneNode(true);
+clone.id = 'roller2';
+document.querySelector('.TopEvent').appendChild(clone);
+document.querySelector('#roller1').style.left = '0px';
+document.querySelector('#roller2').style.left = document.querySelector('.Rolling-list ul').offsetWidth + 'px';
+
+roller.classList.add('original');
+clone.classList.add('clone');
+
+
+const mainImg = document.querySelectorAll('.slide');
+
+function ShowImage(idx, item) {
+  idx += 6;
+  if (idx > 12)
+    idx = (idx) % 12;
+  item.src = `../public/image/man${idx}.jpg`;
+  setTimeout(() => ShowImage(idx, item), 5000);
+}
+mainImg.forEach((item, idx) => {
+  if (idx > 12)
+    item.src = `../public/image/man${idx + 1}.jpg`;
+  setTimeout(() => ShowImage(idx + 1, item), 5000);
+
+})
+
+const sliderContainer = document.querySelector("#slider-items");
+const slidesCount = document.querySelectorAll(".slide").length;
+let currentIdx = 1;
+if (sliderContainer) {
+  function movingSlider(index) {
+    sliderContainer.style.transition = "transform 0.5s ease";
+    sliderContainer.style.transform = `translateX(-${605 * index}px)`;
+  }
+  function AutoSlider() {
+    currentIdx = (currentIdx + 1) % slidesCount;
+    if (currentIdx > 3) {
+      currentIdx = 0;
+    }
+    movingSlider(currentIdx);
+  }
+  document.addEventListener("click", (e) => {
+    let index = currentIdx;
+    e.preventDefault();
+    if (e.target.classList.contains("lBtn")) {
+      currentIdx = currentIdx - 1 >= 0 ? currentIdx - 1 : slidesCount - 3;
+    } else if (e.target.classList.contains("rBtn")) {
+      currentIdx = currentIdx + 3 >= slidesCount ? 0 : currentIdx + 1;
+    }
+    movingSlider(index);
+  });
+  setInterval(AutoSlider, 8000);
+}
+
+
+document.querySelector(".mypage").addEventListener("click", () => {
+  window.location.href = "login.html";
+});
+document.querySelector(".home").addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+if (document.querySelector("#SignM")) {
+  document.querySelector("#SignM").addEventListener("click", () => {
+    window.location.href = "Signup.html";
+  });
+}
+if (document.querySelector("#loginM")) {
+  document.querySelector("#loginM").addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
+}
+
